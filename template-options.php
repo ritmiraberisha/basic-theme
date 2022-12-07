8f503d92b3e715b13531a840fa25c9094713cdb2
ritmiraberisha <ritmiraberisha@gmail.com>
2022-12-07 15:40:04 +0100

    integrating different fields in wordpress

--- template-options.php
+++ template-options.php
@@ -3,6 +3,10 @@
 /* Template Name: Options */
 
 $color = get_field("choose_a_color");
+$flavors = get_field("choose_a_flavor");
+$consent = get_field("do_you_consent");
+$where = get_field("where_do_you_want_to_go");
+$question = get_field("are_you_leaning_anything");
 
 get_header();
 ?>
@@ -11,6 +15,8 @@ get_header();
 <section class="page">
     <div class="container">
 
+                <!-- Select field -->
+                
                 <h1><?php the_title(); ?></h1>
 
                 <?php if($color): ?>
@@ -24,6 +30,35 @@ get_header();
                         echo 'Success! The color is green :)'; 
                     break;
                 };?>
+
+                <br><br>
+
+                <!-- Checkboxes -->
+
+                <?php echo implode($flavors);?>
+
+                <br><br>
+
+                <!-- Radio Buttons -->
+
+                <?php echo $consent;?>
+
+                <br><br>
+
+                <!-- Button Group -->
+
+                <?php echo $where;?>
+
+                <br><br>
+
+                <!-- True/False fields-->
+
+                <?php if($question):?>
+                    Yes, I'm learning :)
+
+                <?php else:?>
+                    No, I'm not learning :(
+                <?php endif; ?>
     </div>
 </section>
 
