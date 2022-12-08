e4fe5e786e335dd464b697462b599bd132044211
ritmiraberisha <ritmiraberisha@gmail.com>
2022-12-08 15:36:56 +0100

    integrated links and page-links

--- single-locations.php
+++ single-locations.php
@@ -1,6 +1,7 @@
 <?php
 
-$locations = get_field("locations");
+$link = get_field("link");
+$page_link = get_field("page_link");
 
 get_header();
 ?>
@@ -10,26 +11,19 @@ get_header();
 
   <h1><?php the_title(); ?></h1>
 
-  <?php
-  $args = [
-    "post_type" => "team",
-    "meta_query" => [
-      "key" => "locations",
-      "value" => '"' . get_the_ID() . '"',
-      "compare" => "LIKE",
-    ],
-  ];
+  <pre><?php echo print_r($link); ?></pre>
 
-  $team_members = get_posts($args);
-  ?>
+  <?php if ($link): ?>
 
-<?php foreach ($team_members as $member): ?>
-    
-    <a href="<?php echo get_permalink(
-      $member->ID
-    ); ?>"><?php echo $member->post_title; ?></a>
+    <a href="<?php echo $link["url"]; ?>" target="<?php echo $link[
+  "target"
+]; ?>"><?php echo $link["title"]; ?></a>
 
-  <?php endforeach; ?>
+    <?php endif; ?>
+
+    <br> <br>
+
+    <a href="<?php echo $page_link; ?>" target="blank">Click here</a>
 
   </div>
 </section>
