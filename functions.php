9a83c6f6a21d7dfcfc77756b7f51415678138006
ritmiraberisha <ritmiraberisha@gmail.com>
2022-12-08 11:42:35 +0100

    added custom posts

--- functions.php
+++ functions.php
@@ -51,3 +51,68 @@ add_theme_support("post-thumbnails");
 // Image Sizes
 add_image_size("small", 600, 600, false);
 add_image_size("my_custom_size", 1200, 600, true);
+
+// Team Members Custom Post
+function custom_post_type()
+{
+  $labels = [
+    "name" => "Team Members",
+    "singular_name" => "Team Member",
+    "add_new" => "Add Item",
+    "all_items" => "Team Members",
+    "add_new_item" => "Add Item",
+    "edit_item" => "Edit Item",
+    "new_item" => "New Item",
+    "view_item" => "View Item",
+    "search_item" => "Search Members",
+    "not_found" => "No items found",
+    "not_found_in_trash" => "No items found in trash",
+    "parent_item_colon" => "Parent Item",
+  ];
+  $args = [
+    "labels" => $labels,
+    "public" => true,
+    "has_archive" => true,
+    "publicly_queryable" => true,
+    "query_var" => true,
+    "rewrite" => true,
+    "capability_type" => "post",
+    "hierarchical" => false,
+    "supports" => ["title", "editor", "excerpt", "thumbnail", "revisions"],
+    "menu_position" => 25,
+    "exclude_from_search" => false,
+    "menu_icon" => "dashicons-admin-users",
+  ];
+  register_post_type("team-members", $args);
+
+  $labels_1 = [
+    "name" => "Locations",
+    "singular_name" => "Location",
+    "add_new" => "Add Item",
+    "all_items" => "Locations",
+    "add_new_item" => "Add Item",
+    "edit_item" => "Edit Item",
+    "new_item" => "New Item",
+    "view_item" => "View Item",
+    "search_item" => "Search Location",
+    "not_found" => "No items found",
+    "not_found_in_trash" => "No items found in trash",
+    "parent_item_colon" => "Parent Item",
+  ];
+  $args_1 = [
+    "labels" => $labels_1,
+    "public" => true,
+    "has_archive" => true,
+    "publicly_queryable" => true,
+    "query_var" => true,
+    "rewrite" => true,
+    "capability_type" => "post",
+    "hierarchical" => false,
+    "supports" => ["title", "editor", "excerpt", "thumbnail", "revisions"],
+    "menu_position" => 26,
+    "exclude_from_search" => false,
+    "menu_icon" => "dashicons-location",
+  ];
+  register_post_type("locations", $args_1);
+}
+add_action("init", "custom_post_type");
